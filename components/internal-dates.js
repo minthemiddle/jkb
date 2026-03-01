2a3a1f05847449817c2bcdbcc0e91abe5c004b66
Martin Betz <kontakt@martinbetz.eu>
2026-03-01 23:40:00 +0100

    Add: Date

--- components/internal-dates.js
+++ components/internal-dates.js
@@ -7,6 +7,7 @@ class AppInternalDates extends HTMLElement {
           <li>6.3. - 8.3. Probenwochenende</li>
           <li>9.5. Sonderprobe für das Konzert im Kammermusiksaal</li>
           <li>10.5. Konzert im Kammermusiksaal der Philharmonie</li>
+          <li>12.5. Erste Probe für Sommerkonzert und Adventskonzert</li>
           <li>28.6. Sommerkonzert</li>
           <li>7.7. Letzte Probe vor der Sommerpause</li>
           <li>25.8. Erste Probe nach der Sommerpause</li>
